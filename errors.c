cab5b8a3ef3f7fd90c4f8b51cded20232aae2135
GodsonO <godsonomoghene15@gmail.com>
2023-04-21 02:49:59 -0700

    simple shell

--- errors.c
+++ errors.c
@@ -1,85 +1,81 @@
 #include "shell.h"
-
 /**
  *_eputs - prints an input string
- * @str: the string to be printed
- *
+ * @s: the string to be printed
  * Return: Nothing
  */
-void _eputs(char *str)
+void _eputs(char *s)
 {
-	int i = 0;
+	int x = 0;
 
-	if (!str)
-		return;
-	while (str[i] != '\0')
+	if (!s)
 	{
-		_eputchar(str[i]);
-		i++;
+		return;
+	}
+	while (s[x] != '\0')
+	{
+		_eputchar(s[x]);
+		x++;
 	}
 }
-
 /**
  * _eputchar - writes the character c to stderr
- * @c: The character to print
- *
+ * @g: The character to print
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _eputchar(char c)
+int _eputchar(char g)
 {
-	static int i;
+	static int x;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (g == BUF_FLUSH || x >= WRITE_BUF_SIZE)
 	{
-		write(2, buf, i);
-		i = 0;
+		write(2, buf, x);
+		x = 0;
 	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
+	if (g != BUF_FLUSH)
+		buf[x++] = g;
 	return (1);
 }
-
 /**
  * _putfd - writes the character c to given fd
- * @c: The character to print
+ * @g: The character to print
  * @fd: The filedescriptor to write to
- *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putfd(char c, int fd)
+int _putfd(char g, int fd)
 {
-	static int i;
+	static int x;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (g == BUF_FLUSH || x >= WRITE_BUF_SIZE)
 	{
-		write(fd, buf, i);
-		i = 0;
+		write(fd, buf, x);
+		x = 0;
 	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
+	if (g != BUF_FLUSH)
+	{
+		buf[x++] = g;
+	}
 	return (1);
 }
-
 /**
  *_putsfd - prints an input string
- * @str: the string to be printed
+ * @s: the string to be printed
  * @fd: the filedescriptor to write to
- *
  * Return: the number of chars put
  */
-int _putsfd(char *str, int fd)
+int _putsfd(char *s, int fd)
 {
-	int i = 0;
+	int x = 0;
 
-	if (!str)
+	if (!s)
 		return (0);
-	while (*str)
+	while (*s)
 	{
-		i += _putfd(*str++, fd);
+		x += _putfd(*s++, fd);
 	}
-	return (i);
+	return (x);
 }
