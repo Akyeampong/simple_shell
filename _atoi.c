a57988fdbf20097a20c430a0cfdf1874e909308d
GodsonO <godsonomoghene15@gmail.com>
2023-04-20 22:23:32 -0700

    _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -1,65 +1,68 @@
 #include "shell.h"
 
 /**
- * interactive - returns true if shell is interactive mode
+ * interactive - returns true if shell is in interactive mode
  * @info: struct address
- *
- * Return: 1 if interactive mode, 0 otherwise
+ * Return: 1 if in interactive mode,
+ * 0 otherwise
  */
 int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
-
 /**
  * is_delim - checks if character is a delimeter
- * @c: the char to check
+ * @g: the char to check
  * @delim: the delimeter string
  * Return: 1 if true, 0 if false
  */
-int is_delim(char c, char *delim)
+int is_delim(char g, char *delim)
 {
 	while (*delim)
-		if (*delim++ == c)
+	{
+		if (*delim++ == g)
+		{
 			return (1);
+		}
+	}
 	return (0);
 }
-
 /**
  *_isalpha - checks for alphabetic character
- *@c: The character to input
- *Return: 1 if c is alphabetic, 0 otherwise
+ *@g: The character to input
+ *Return: 1 if g is alphabetic, 0 otherwise
  */
 
-int _isalpha(int c)
+int _isalpha(int g)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((g >= 'a' && g <= 'z') || (g >= 'A' && g <= 'Z'))
+	{
 		return (1);
+	}
 	else
 		return (0);
 }
-
 /**
  *_atoi - converts a string to an integer
- *@s: the string to be converted
+ *@str: the string to be converted
  *Return: 0 if no numbers in string, converted number otherwise
  */
 
-int _atoi(char *s)
+int _atoi(char *str)
 {
-	int i, sign = 1, flag = 0, output;
+	int sign = 1, flag = 0, output, x;
 	unsigned int result = 0;
 
-	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	for (x = 0;  str[x] != '\0' && flag != 2; x++)
 	{
-		if (s[i] == '-')
+		if (str[x] == '-')
 			sign *= -1;
 
-		if (s[i] >= '0' && s[i] <= '9')
+		if (str[x] >= '0' && str[x] <= '9')
 		{
 			flag = 1;
 			result *= 10;
-			result += (s[i] - '0');
+			result += (str[x] - '0');
 		}
 		else if (flag == 1)
 			flag = 2;
